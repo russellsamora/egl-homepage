01ec6f2097f4fc60ad961059a38333f5bf976d0b
russell goldenberg <russellgoldenberg@gmail.com>
2013-05-21 12:12:57 -0400

    animated walk cycle, data loading

--- js/game.js
+++ js/game.js
@@ -3,9 +3,16 @@ var width,
 	player,
 	inTransit = false,
 	gameboard,
+	gameboardWidth,
+	gameboardHeight,
 	scrollElement,
 	maxScroll,
-	navBarHeight = 64;
+	navBarHeight = 64,
+	direction,
+	currentFrame,
+	numFrames = 2,
+	gameData,
+	ready = false;
 
 $(function() {
 	setupSelectors();
@@ -13,10 +20,13 @@ $(function() {
 	setupEvents();
 	resize();
 	setPosition();
+	loadData('backup');
 });
 
 function setupSelectors() {
 	gameboard = $('#gameboard');
+	gameboardWidth = parseInt(gameboard.css('width'),10);
+	gameboardHeight = parseInt(gameboard.css('height'),10);
 	scrollElement = $('html, body');
 	scrollElement.each(function(i) {
         $(this).scrollTop(0).scrollLeft(0);
@@ -27,11 +37,11 @@ function setupPlayer() {
 	player = {
 		selector: $('#player'),
 		position: {},
-		width: 128,
-		height: 256,
+		width: 80,
+		height: 160,
 		offset: {
-			x: 64,
-			y: 128
+			x: 40,
+			y: 80
 		}
 	};
 }
@@ -40,9 +50,17 @@ function setupEvents() {
 	gameboard.on('click', function(e) {
 		e.preventDefault();
 		if(!inTransit) {
+			//constrain to the left and top screen
+			var y = Math.max(0,(e.pageY - navBarHeight - player.offset.y)),
+				x = Math.max(0,e.pageX - player.offset.x);
+
+			//constrain bottom and right of screen
+			x = Math.min(x, gameboardWidth - player.width);
+			y = Math.min(y, gameboardHeight - player.height);
+
 			var input = {
-				x: (e.pageX - player.offset.x),
-				y: (e.pageY - navBarHeight - player.offset.y),
+				x: x,
+				y: y,
 				edgeX: e.clientX,
 				edgeY: e.clientY
 			};
@@ -50,6 +68,7 @@ function setupEvents() {
 			movePlayer(input);
 		}
 	});
+	$(window).on('resize', resize);
 }
 
 function setPosition() {
@@ -65,17 +84,43 @@ function resize() {
 	width = $(window).width();
 	height = $(window).height();
 	maxScroll = { 
-		left: Math.max(0,parseInt(gameboard.css('width'),10) - width),
-		top: Math.max(0,parseInt(gameboard.css('height'),10) - height)
+		left: Math.max(0,gameboardWidth - width),
+		top: Math.max(0,gameboardHeight - height + navBarHeight)
 	};
 }
 
 function movePlayer(input) {
 	inTransit = true;
-	var distance = (Math.abs(player.position.x - input.x) + Math.abs(player.position.y - input.y)) / 2,
-		speed = distance * 5;
+
+	//do some spatial calculations to find distance and speed
+	var diffX =  input.x - player.position.x,
+		diffY = input.y - player.position.y,
+		absDiffX = Math.abs(diffX),
+		absDiffY = Math.abs(diffY),
+		distance =  (absDiffX + absDiffY) / 2,
+		speed = distance * 10;
 
 	input.speed = speed;
+	
+	//calculate direction
+	//this means its going pretty vertical likely (images: left, right, down, up, idle)
+	if(absDiffY / absDiffX > 1) {
+		if(diffY > 0) {
+			//down
+			direction = 320;
+		} else {
+			//up
+			direction = 480;
+		}
+	} else {
+		if(diffX > 0) {
+			//right
+			direction = 160;
+		} else {
+			//left
+			direction = 0;
+		}
+	}
 	slideScreen(input);
 	player.selector.animate({
 		top: input.y,
@@ -84,7 +129,25 @@ function movePlayer(input) {
 		inTransit = false;
 		player.position.x = input.x;
 		player.position.y = input.y;
+		player.selector.css({
+			'background-position': -640
+		});
 	});
+
+	currentFrame = -1;
+	animateWalkCycle();
+}
+
+function animateWalkCycle() {
+	if(inTransit) {
+		currentFrame++;
+		if(currentFrame >= numFrames) {
+			currentFrame = 0;
+		}
+		var pos = -(direction + currentFrame * player.width) + 'px';
+		player.selector.css('background-position', pos);
+		setTimeout(animateWalkCycle, 170);
+	}
 }
 
 function slideScreen(input) {
@@ -107,6 +170,7 @@ function slideScreen(input) {
 	} else if( input.edgeY > height - player.height) {
 		destY = Math.min(pageYOffset + height / 2, maxScroll.top);
 	}
+	//console.log('y:', input.y,'edge:', input.edgeY,'page:', pageYOffset,'max:', maxScroll.top);
 
 	//choose which to animate (must lump together so it doesn't halt other)
 	if(destX !== undefined && destY !== undefined) {
@@ -123,4 +187,32 @@ function slideScreen(input) {
 			scrollLeft: destX
 		}, speed,'linear');	
 	}
+}
+
+function loadData(backupData) {
+	if(backupData) {
+		gameData = new Miso.Dataset({
+			url: '../data/backup.csv',
+			delimiter: ','
+		});
+	} else {
+		gameData = new Miso.Dataset({
+			importer : Miso.Dataset.Importers.GoogleSpreadsheet,
+			parser : Miso.Dataset.Parsers.GoogleSpreadsheet,
+			key : '0AtnV9m5qu78_dEY2dWNIRXNhTk1USk9rRG9McTFuMkE',
+			worksheet: '1'
+		});
+	}
+	gameData.fetch({ 
+		success : function() {
+			this.each(function(row){
+				console.log(row);
+			});
+			ready = true;
+		},
+		error : function() {
+			console.log('having a bad day? Try backup data!');
+			loadData('backup');
+		}
+	});
 }
