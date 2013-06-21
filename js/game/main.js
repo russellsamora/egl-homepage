d9a495b74d9f443713a138a6afba4b07c3061756
russell goldenberg <russellgoldenberg@gmail.com>
2013-06-21 16:03:41 -0400

    woot modular loading with labjs and some style

--- js/game/main.js
+++ js/game/main.js
@@ -1,6 +1,14 @@
-$(function() {
-	//determine if we are playing the game based on some css attribute?
-	//also can do a modernizr check here to load in specific files
+//determine if we are playing the game based on some css attribute?
+//also can do a modernizr check here to load in specific files
 
-	//load files now that stuff is approved and dom is loaded.
-});
+//load files now that stuff is approved and dom is loaded.
+(function() {
+	var self = window.game = {
+
+		init: function() {
+			console.log('init');
+		}
+	};
+
+	self.init();
+})();
