8c964864af8c831a3ed6b0fe21b1dd0e6106afb8
russell goldenberg <russellgoldenberg@gmail.com>
2013-08-23 15:09:25 -0400

    courses

--- resources/index.php
+++ resources/index.php
@@ -18,14 +18,14 @@
 					<div class='row'>
 						<div class='span12'>
 							<h2>
-								VM420: Games and Social Change
+								VM-420: Games and Social Change
 							</h2>
 						</div>
 					</div>
 					<div class='row'>
 						<div class='span12 professor'>
 							<p>
-								Professor Eric Gordon
+								PROFESSOR ERIC GORDON
 							</p>
 						</div>
 					</div>
@@ -33,6 +33,30 @@
 						<div class='span12'>
 							<p>
 								The video game industry is one of the largest sectors of the entertainment industry. But the importance of games goes well beyond industrial silos. Games and game mechanics are factoring into the experience of television, film and the web, and increasingly, into other institutions, including education, democracy and health. What's more, games are being deployed in these contexts to produce real world social change. This class provides students with an introduction to games and game design and gives them the opportunity to partner with the Red Cross / Red Crescent in designing games for real people to solve real humanitarian problems. Students will work in groups, each with a specific geographic and content focus, to design board (or otherwise analog) games and digital prototypes that will be tested and deployed. Prerequisites: Junior standing and some programming or design experience is ideal, but not required.
+							</p>
+						</div>
+					</div>
+					<div class='row'>
+						<div class='span12'>
+							<h2>
+								IN-374: Civic Media
+							</h2>
+						</div>
+					</div>
+					<div class='row'>
+						<div class='span12 professor'>
+							<p>
+								PROFESSOR JESSE BALDWIN-PHILIPPI
+							</p>
+						</div>
+					</div>
+					<div class='row'>
+						<div class='span12'>
+							<p>
+								Participating in local life can be difficult. Information is hard to obtain and validate, local meetings are difficult to attend, networks are challenging to build. Increasingly, governments, advocacy groups, community organizers, and individual citizens are looking to digital tools to increase and improve the conditions in which we live and enhance our opportunities to engage.  From electoral politics to global advocacy campaigns to local education planning, digital tools are seen (and often touted!) as the solution, but what actual effects are they having? How are their goals and outcomes any different than what came before? This course uses these overarching questions to ground its investigation of many civic media interventions over time, across diverse contexts, and with numerous goals.
+							</p>
+							<p>
+								This class will not only explore the various goals campaigns are using digital tools to meet, but will also focus on what type of citizen these tools are enabling and encouraging people to become. We will look at academic research surrounding citizenship and engagement in a digital era and cover research into many genres of civic media, from citizen journalism to hackathons. Additionally, it will focus on questions of design: How best can we, as media creators, encourage certain behaviors? What type of citizens are we building when we make design choices?
 							</p>
 						</div>
 					</div>
